158d7734e9b31021cc46a3ac8941210c3cfb6126
Sikander <skhan.bese23seecs@seecs.edu.pk>
2026-08-22 22:09:35 +0500

    feat(typography): configure custom fonts and global CSS design tokens

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,36 @@
+import localFont from "next/font/local";
 import "./globals.css";
 import Nav from "@/components/nav";
+
+const broela = localFont({
+  src: "../public/fonts/broela/Broela.otf",
+  variable: "--font-broela",
+  display: "swap",
+});
+
+const calluna = localFont({
+  src: "../public/fonts/calluna/Calluna-Regular.woff",
+  variable: "--font-calluna",
+  display: "swap",
+});
+
+const nomad = localFont({
+  src: "../public/fonts/nomad/Nomad.otf",
+  variable: "--font-nomad",
+  display: "swap",
+});
+
+const macSans = localFont({
+  src: "../public/fonts/mac_sans/MacSans-Regular.otf",
+  variable: "--font-macsans",
+  display: "swap",
+});
+
+const prodaSans = localFont({
+  src: "../public/fonts/proda_sans/ProdaSans-Regular.woff2",
+  variable: "--font-proda",
+  display: "swap",
+});
 
 export const metadata = {
   title: "Thriftable",
@@ -8,7 +39,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="h-full antialiased">
+    <html lang="en" className={`${broela.variable} ${calluna.variable} ${nomad.variable} ${macSans.variable} ${prodaSans.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col bg-white text-neutral-900">
         <Nav />
         <main className="flex-1">{children}</main>
@@ -16,3 +47,4 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
